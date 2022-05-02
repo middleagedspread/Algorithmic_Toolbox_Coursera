30498c7680a3cd807af26f040f95920ae7096df5
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-01 18:32:31 -0600

    Updated algorithms including adding fast fibonacci. Added fibbonacci looping

--- Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
+++ Algorithmic Warm Up/Fibonacci Number/fibonacci_number.py
@@ -1,6 +1,7 @@
 # python3
-fibonacci_dict = {}
 
+import time
+fibonacci_dict = {}
 
 def fibonacci_number_naive(n):
     assert 0 <= n <= 45
@@ -10,8 +11,8 @@ def fibonacci_number_naive(n):
     return fibonacci_number_naive(n - 1) + fibonacci_number_naive(n - 2)
 
 
-def fibonacci_number(n):
-    assert 0 <= n <= 45
+def fibonacci_number(n):  # recursion depth maxes out after about 500 iterations
+    assert 0 <= n <= 500
     if n <= 1:  # base case of 0 or 1
         return n
     if n not in fibonacci_dict:  # if we haven't already computed fibonacci(n), add it to the dictionary
@@ -19,14 +20,26 @@ def fibonacci_number(n):
     return fibonacci_dict[n]
 
 
-def fibonacci_number_list(n):
+def fibonacci_number_list(n):  # about 10 times faster than the recursive method
     assert 0 <= n <= 10**6
     fibonacci_list = [0, 1]
     for i in range(2, n+1):
         fibonacci_list.append(fibonacci_list[i-1]+fibonacci_list[i-2])
     return fibonacci_list[n]
 
+def speed_test():
+    start_time = time.time()
+    print(fibonacci_number(500))
+    print("--- %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
+    print(fibonacci_number_list(5000))
+    print("--- %s seconds ---" % (time.time() - start_time))
 
 if __name__ == '__main__':
     input_n = int(input())
     print(fibonacci_number(input_n))
+
+# print(fibonacci_number_naive(35))
+
+
+
--- Algorithmic Warm Up/Greatest Common Divisor/gcd.py
+++ Algorithmic Warm Up/Greatest Common Divisor/gcd.py
@@ -11,10 +11,14 @@ def gcd_naive(a, b):
     assert False
 
 
-def gcd(a, b):
+def gcd(a, b):  # Euclid's algorithm for finding the GCD of two numbers
     assert 0 <= a <= 2 * 10 ** 9 and 0 <= b <= 2 * 10 ** 9
 
-    type here
+    if b == 0:  # no remainder on previous recursion
+        return a  # the GCD
+    else:
+        a_prime = a % b  # a_prime is the remainder left when b divides a
+        return gcd(b, a_prime)  # recursion
 
 
 if __name__ == '__main__':
