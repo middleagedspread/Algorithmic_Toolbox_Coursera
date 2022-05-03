64496d6f154c461a5e8b068ef5cad35848bbb519
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-03 17:13:58 -0600

    Updated maximum loot to create greedy algorithm for fractional knapsack problem

--- Greedy Algorithms/Maximum Value of the Loot/maximum_loot.py
+++ Greedy Algorithms/Maximum Value of the Loot/maximum_loot.py
@@ -10,7 +10,21 @@ def maximum_loot_value(capacity, weights, prices):
     assert all(0 < w <= 2 * 10 ** 6 for w in weights)
     assert all(0 <= p <= 2 * 10 ** 6 for p in prices)
 
-    type here
+    if capacity == 0 or not weights:
+        return 0
+
+    max_index = prices.index(max(prices))
+    # print(f"max_index: {max_index}")
+    amount = min(weights[max_index],capacity)
+    # print(f"amount: {amount}")
+    value = prices[max_index] * amount / weights[max_index]
+    # print(f"value: {value}")
+    capacity -= amount
+    # print(f"new capacity: {capacity}")
+    weights.pop(max_index)
+    prices.pop(max_index)
+    return value + maximum_loot_value(capacity, weights, prices)
+
 
 
 if __name__ == "__main__":
@@ -20,3 +34,6 @@ if __name__ == "__main__":
     input_weights = data[3:(2 * n + 2):2]
     opt_value = maximum_loot_value(input_capacity, input_weights, input_prices)
     print("{:.10f}".format(opt_value))
+
+# print (maximum_loot_value(10, [30], [500]))
+
