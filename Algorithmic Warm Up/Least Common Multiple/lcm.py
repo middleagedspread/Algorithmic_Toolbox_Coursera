8e35d2ae109c1a323c53067f54124d119c48a759
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-03 10:28:00 -0600

    Updating least common multiple algorithm

--- Algorithmic Warm Up/Least Common Multiple/lcm.py
+++ Algorithmic Warm Up/Least Common Multiple/lcm.py
@@ -1,4 +1,5 @@
 # python3
+import time
 
 
 def lcm_naive(a, b):
@@ -11,12 +12,30 @@ def lcm_naive(a, b):
     return multiple
 
 
-def lcm(a, b):
+# def lcm(a, b):
+#     assert 1 <= a <= 2 * 10 ** 9 and 1 <= b <= 2 * 10 ** 9
+#     larger, smaller = max(a,b), min(a,b)
+#     multiple = larger
+#     while multiple % smaller != 0:
+#         multiple += larger
+#     return multiple
+
+def gcd(a, b):  # Euclid's algorithm for finding the GCD of two numbers
+    assert 0 <= a <= 2 * 10 ** 9 and 0 <= b <= 2 * 10 ** 9
+
+    if b == 0:  # no remainder on previous recursion
+        return a  # the GCD
+    else:
+        a_prime = a % b  # a_prime is the remainder left when b divides a
+        return gcd(b, a_prime)  # recursion
+
+def lcm(a, b):  # using Euclid's GCD
     assert 1 <= a <= 2 * 10 ** 9 and 1 <= b <= 2 * 10 ** 9
+    larger, smaller = max(a,b), min(a,b)
+    return int(smaller * larger / gcd(larger,smaller))
 
-    type here
+# if __name__ == '__main__':
+#     input_a, input_b = map(int, input().split())
+#     print(lcm(input_a, input_b))
 
-
-if __name__ == '__main__':
-    input_a, input_b = map(int, input().split())
-    print(lcm(input_a, input_b))
+print(lcm(47,64))
