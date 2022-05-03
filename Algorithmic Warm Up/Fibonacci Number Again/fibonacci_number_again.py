177f1ab0948d711b72ff338b4e4cd3589d639c0e
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-03 11:58:03 -0600

    Used algorithm for calculating Pisano numbers to get last digit of F(n) mod m for 2<m<1000

--- Algorithmic Warm Up/Fibonacci Number Again/fibonacci_number_again.py
+++ Algorithmic Warm Up/Fibonacci Number Again/fibonacci_number_again.py
@@ -13,13 +13,35 @@ def fibonacci_number_again_naive(n, m):
 
     return current
 
+def pisano_period(m): # for any integer m≥2, the sequence Fn mod m is periodic
+    previous, current = 0, 1
+    for i in range(0, m * m): # pisano period bound by 0 and m * m
+        previous, current = current, (previous + current) % m # calculate the fibonacci sequence
+        # as soon as the sequence returns to ...,0,1,... it will repeat
+        # A Pisano Period starts with 0,1
+        if (previous == 0 and current == 1):
+            return i + 1
 
 def fibonacci_number_again(n, m):
     assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 3
 
-    type here
+    pisano_number = pisano_period(m)
+
+    if n > pisano_number:  # the last digit of the fibbonaci sequence repeats after 60 cycles
+        n = n % pisano_number
+
+    if n <= 1:
+        return n
+
+    previous, current = 0, 1
+    for _ in range(n - 1):
+        previous, current = current, (previous + current) % m
+    return current
 
 
-if __name__ == '__main__':
-    input_n, input_m = map(int, input().split())
-    print(fibonacci_number_again(input_n, input_m))
+# if __name__ == '__main__':
+#     input_n, input_m = map(int, input().split())
+#     print(fibonacci_number_again(input_n, input_m))
+
+# print(fibonacci_number_again_naive(115,10))
+# print(fibonacci_number_again(2019,5))
--- Algorithmic Warm Up/Last Digit of Fibonacci Number/last_digit_of_fibonacci_number.py
+++ Algorithmic Warm Up/Last Digit of Fibonacci Number/last_digit_of_fibonacci_number.py
@@ -10,14 +10,27 @@ def last_digit_of_fibonacci_number_naive(n):
     return (last_digit_of_fibonacci_number_naive(n - 1) + last_digit_of_fibonacci_number_naive(n - 2)) % 10
 
 
+# def last_digit_of_fibonacci_number(n):  # using a list
+#     assert 0 <= n <= 10 ** 7
+#     if n > 60:  # the last digit of the fibbonaci sequence repeats after 60 cycles
+#         n = n % 60
+#     fibonacci_list = [0, 1]
+#     for i in range(2, n+1):
+#         fibonacci_list.append((fibonacci_list[i-1]+fibonacci_list[i-2])%10)
+#     return fibonacci_list[n]
+
 def last_digit_of_fibonacci_number(n):
     assert 0 <= n <= 10 ** 7
+
     if n > 60:  # the last digit of the fibbonaci sequence repeats after 60 cycles
         n = n % 60
-    fibonacci_list = [0, 1]
-    for i in range(2, n+1):
-        fibonacci_list.append((fibonacci_list[i-1]+fibonacci_list[i-2])%10)
-    return fibonacci_list[n]
+    if n <= 1:
+        return n
+    previous, current = 0, 1
+    for _ in range(n - 1):
+        previous, current = current, (previous + current) % 10
+
+    return current
 
 
 if __name__ == '__main__':
@@ -26,12 +39,12 @@ if __name__ == '__main__':
 
 # first_sixty = []
 # second_sixty = []
-# for i in range(1,60):
+# for i in range(200,250):
 #     first_sixty.append(last_digit_of_fibonacci_number(i))
 #
-# for i in range(61,120):
-#     second_sixty.append(last_digit_of_fibonacci_number(i))
+# for i in range(200, 250):
+#     second_sixty.append(last_digit_of_fibonacci_number_try(i))
 #
-# print(first_sixty)
-# print(second_sixty)
+# print(last_digit_of_fibonacci_number(240))
+# print(last_digit_of_fibonacci_number_try(240))
 
