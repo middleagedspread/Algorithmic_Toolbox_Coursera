6a2992b8a240f9d8421e9e39fd3282ec3b759275
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-03 12:52:35 -0600

    Updated sum of Fibonacci numbers from 1-n using algorithm employing periodicity (Pisani number of F(n) mod m same as pisani number of Sum from 1-n Fn mod m).

--- Algorithmic Warm Up/Last Digit of the Sum of Fibonacci Numbers/last_digit_of_the_sum_of_fibonacci_numbers.py
+++ Algorithmic Warm Up/Last Digit of the Sum of Fibonacci Numbers/last_digit_of_the_sum_of_fibonacci_numbers.py
@@ -19,9 +19,28 @@ def last_digit_of_the_sum_of_fibonacci_numbers_naive(n):
 def last_digit_of_the_sum_of_fibonacci_numbers(n):
     assert 0 <= n <= 10 ** 18
 
-    type here
+    # the Pisano period (Sum F0+F1+⋯+Fn) mod m is the same as the Pisano period for F(n) mod m
+    pisano_number = 60  # for base 10
+
+    if n > pisano_number:  # the last digit of the fibbonaci sequence repeats after 60 cycles
+        n = n % pisano_number
+
+    if n <= 1:
+        return n
+
+    fibonacci_numbers = [0] * (n + 1)
+    fibonacci_numbers[0] = 0
+    fibonacci_numbers[1] = 1
+    for i in range(2, n + 1):
+        fibonacci_numbers[i] = fibonacci_numbers[i - 2] + fibonacci_numbers[i - 1]
+
+    return sum(fibonacci_numbers) % 10
+
+
+
 
 
 if __name__ == '__main__':
     input_n = int(input())
     print(last_digit_of_the_sum_of_fibonacci_numbers(input_n))
+
