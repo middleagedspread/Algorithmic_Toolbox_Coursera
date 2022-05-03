8a114dc614e258f5fcb2f1f5a11a88e866c067bd
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-03 16:20:33 -0600

    Implemented recursive car fill greedy algorithm. tested speed against other version

--- Greedy Algorithms/Car Fueling/car_fueling.py
+++ Greedy Algorithms/Car Fueling/car_fueling.py
@@ -1,7 +1,7 @@
 # python3
 import time
 
-def compute_min_number_of_refills(d, m, stops):
+def compute_min_number_of_refills_mine(d, m, stops):
     assert 1 <= d <= 10 ** 5
     assert 1 <= m <= 400
     assert 1 <= len(stops) <= 300
@@ -20,16 +20,68 @@ def compute_min_number_of_refills(d, m, stops):
         current_position = stops[index]
         min_stops +=1
     return min_stops
+#
 
 
+def refills(distance, gas_range, stops, location=0):
+    # print(f"distance: {distance}")
+    # print(f"stops[0] - location: {stops[0] - location}")
+    # print(f"gas range: {gas_range}")
+    # print(f"stops: {stops}")
+    # print(f"location: {location}")
+
+    if location + gas_range >= distance:
+        return 0
+
+    if not stops or stops[0] - location > gas_range:
+        return -1
+
+    last_stop = location
+
+    while stops and (stops[0] - location <= gas_range):
+        last_stop = stops[0]
+        stops.pop(0)
+
+    next_stop = refills(distance, gas_range, stops, last_stop)
+    if next_stop == -1:
+        return -1
+    else:
+        return 1 + next_stop
 
 
-if __name__ == '__main__':
-    input_d = int(input())
-    input_m = int(input())
-    input_n = int(input())
-    input_stops = list(map(int, input().split()))
-    assert len(input_stops) == input_n
+def compute_min_number_of_refills(d, m, stops):
+    assert 1 <= d <= 10 ** 5
+    assert 1 <= m <= 400
+    assert 1 <= len(stops) <= 300
+    assert 0 < stops[0] and all(stops[i] < stops[i + 1] for i in range(len(stops) - 1)) and stops[-1] < d
 
-    print(compute_min_number_of_refills(input_d, input_m, input_stops))
+    return refills(d, m, stops)
+
+
+# if __name__ == '__main__':
+#     input_d = int(input())
+#     input_m = int(input())
+#     input_n = int(input())
+#     input_stops = list(map(int, input().split()))
+#     assert len(input_stops) == input_n
+#
+#     print(compute_min_number_of_refills(input_d, input_m, input_stops))
+
+tic = time.perf_counter()
+print(compute_min_number_of_refills(500, 200, [100, 200, 300, 400]))
+print(compute_min_number_of_refills(950, 400, [200, 375, 550, 750]))
+print(compute_min_number_of_refills(10, 3, [1, 2, 5, 9]))
+print(compute_min_number_of_refills(200, 250, [100, 150]))
+print(compute_min_number_of_refills(100, 120, [30, 60, 80]))
+toc = time.perf_counter()
+print(f"Ran the code in {toc - tic:0.8f} seconds")
+
+tic = time.perf_counter()
+print(compute_min_number_of_refills_mine(500, 200, [100, 200, 300, 400]))
+print(compute_min_number_of_refills_mine(950, 400, [200, 375, 550, 750]))
+print(compute_min_number_of_refills_mine(10, 3, [1, 2, 5, 9]))
+print(compute_min_number_of_refills_mine(200, 250, [100, 150]))
+print(compute_min_number_of_refills_mine(100, 120, [30, 60, 80]))
+toc = time.perf_counter()
+print(f"Ran the code in {toc - tic:0.8f} seconds")
 
