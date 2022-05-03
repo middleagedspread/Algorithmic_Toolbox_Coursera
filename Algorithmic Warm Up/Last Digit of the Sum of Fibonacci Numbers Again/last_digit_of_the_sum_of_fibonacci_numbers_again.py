f8ff28e196e71482fb03e0e8b76756cafb133c69
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-03 13:12:53 -0600

    Updated last digit of sum of fibonacci numbers again file to uncomment main()

--- Algorithmic Warm Up/Last Digit of the Sum of Fibonacci Numbers Again/last_digit_of_the_sum_of_fibonacci_numbers_again.py
+++ Algorithmic Warm Up/Last Digit of the Sum of Fibonacci Numbers Again/last_digit_of_the_sum_of_fibonacci_numbers_again.py
@@ -18,10 +18,24 @@ def last_digit_of_the_sum_of_fibonacci_numbers_again_naive(from_index, to_index)
 
 def last_digit_of_the_sum_of_fibonacci_numbers_again(from_index, to_index):
     assert 0 <= from_index <= to_index <= 10 ** 18
+    from_index = from_index % 60  #  Periodicity of last digit F(n) in base 10 is 60
+    to_index = to_index % 60
 
-    type here
+    if to_index == 0:
+        return 0
+
+    fibonacci_numbers = [0] * (to_index + 1)
+    fibonacci_numbers[0] = 0
+    fibonacci_numbers[1] = 1
+    for i in range(2, to_index + 1):
+        fibonacci_numbers[i] = fibonacci_numbers[i - 2] + fibonacci_numbers[i - 1]
+
+    return sum(fibonacci_numbers[from_index:to_index + 1]) % 10
 
 
 if __name__ == '__main__':
     input_from, input_to = map(int, input().split())
     print(last_digit_of_the_sum_of_fibonacci_numbers_again(input_from, input_to))
+
+# print(last_digit_of_the_sum_of_fibonacci_numbers_again(10,10))
+# print(last_digit_of_the_sum_of_fibonacci_numbers_again(100,200))
