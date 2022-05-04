0b99e5a4cd7af24a80ad9dc0685d4746d8ac4a81
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-04 17:42:09 -0600

    Fixed maximum loot greedy algorithm for fractional knapsack problem

--- Greedy Algorithms/Maximum Value of the Loot/maximum_loot.py
+++ Greedy Algorithms/Maximum Value of the Loot/maximum_loot.py
@@ -10,30 +10,34 @@ def maximum_loot_value(capacity, weights, prices):
     assert all(0 < w <= 2 * 10 ** 6 for w in weights)
     assert all(0 <= p <= 2 * 10 ** 6 for p in prices)
 
-    if capacity == 0 or not weights:
-        return 0
+    length = int(len(weights))
+    loot = 0
+    value_list = []  # create list to hold weights, prices, and price/weight
 
-    max_index = prices.index(max(prices))
-    # print(f"max_index: {max_index}")
-    amount = min(weights[max_index],capacity)
-    # print(f"amount: {amount}")
-    value = prices[max_index] * amount / weights[max_index]
-    # print(f"value: {value}")
-    capacity -= amount
-    # print(f"new capacity: {capacity}")
-    weights.pop(max_index)
-    prices.pop(max_index)
-    return value + maximum_loot_value(capacity, weights, prices)
+    for i in range(0, length):  # O(n)
+        value_list.append([prices[i], weights[i], prices[i]/weights[i]])
+
+    # sort the list on the price / weight, ascending
+    value_list.sort(key=lambda x: x[2], reverse=True)  # O(n log n)
+
+    i = 0
+    while capacity > 0 and i < length:  # O(n)
+        amount = min(value_list[i][1], capacity)  # get the next amount
+        loot += amount * value_list[i][2]  # increment the loot
+        capacity -= amount  # decrement the remaining capacity
+        i += 1  # move to the next element of the list
+
+    return loot
 
 
+# if __name__ == "__main__":
+#     data = list(map(int, stdin.read().split()))
+#     n, input_capacity = data[0:2]
+#     input_prices = data[2:(2 * n + 2):2]
+#     input_weights = data[3:(2 * n + 2):2]
+#     opt_value = maximum_loot_value(input_capacity, input_weights, input_prices)
+#     print("{:.10f}".format(opt_value))
 
-if __name__ == "__main__":
-    data = list(map(int, stdin.read().split()))
-    n, input_capacity = data[0:2]
-    input_prices = data[2:(2 * n + 2):2]
-    input_weights = data[3:(2 * n + 2):2]
-    opt_value = maximum_loot_value(input_capacity, input_weights, input_prices)
-    print("{:.10f}".format(opt_value))
-
+# print(maximum_loot_value(50, [20, 50, 30], [60, 100, 120]))
 # print (maximum_loot_value(10, [30], [500]))
-
+# print(maximum_loot_value(100, [30, 40, 20], [60, 100, 120]))
