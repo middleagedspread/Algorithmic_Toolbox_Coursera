f9ad30b56e4b24f9036137c94ec4df7c57584397
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-05 16:39:56 -0600

    Created algorithm to return minimum set of points such that each segment contains a point

--- Greedy Algorithms/Collecting Signatures/collecting_signatures.py
+++ Greedy Algorithms/Collecting Signatures/collecting_signatures.py
@@ -5,9 +5,24 @@ from sys import stdin
 
 Segment = namedtuple('Segment', 'start end')
 
-
+# finds the minimum number of points such that all segments contain a point
 def compute_optimal_points(segments):
-    type here
+    # sort the segments by rightmost point, ascending
+    # print(segments)
+    # segments.sort(key=lambda x: (x.end, -x.start))
+    segments.sort(key=lambda x: x.end)  # O (n log n)
+    # print(segments)
+    next_point = segments[0].end
+    points = [next_point]
+    # print(next_point)
+    for i in range(1,len(segments)):  # O(n)
+        # if the point doesn't overlap this segment
+        if not (segments[i].start <= next_point <= segments[i].end):
+            # update next point to end of this segment and add to points
+            next_point = segments[i].end
+            points.append(next_point)
+
+    return points
 
 
 if __name__ == '__main__':
@@ -17,3 +32,7 @@ if __name__ == '__main__':
     output_points = compute_optimal_points(input_segments)
     print(len(output_points))
     print(*output_points)
+
+# print(compute_optimal_points([Segment(1, 3), Segment(2, 5), Segment(3, 6)]))
+# print(compute_optimal_points([Segment(4, 7), Segment(1, 3), Segment(2, 5), Segment(5, 6)]))
+
