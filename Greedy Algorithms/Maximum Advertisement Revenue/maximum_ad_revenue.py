f0f704c8b033e79426b91d208800644468535641
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-05 15:29:28 -0600

    Created faster algorithm for maximum dot product of two lists

--- Greedy Algorithms/Maximum Advertisement Revenue/maximum_ad_revenue.py
+++ Greedy Algorithms/Maximum Advertisement Revenue/maximum_ad_revenue.py
@@ -23,7 +23,12 @@ def max_dot_product(first_sequence, second_sequence):
     assert all(0 <= f <= 10 ** 5 for f in first_sequence)
     assert all(0 <= s <= 10 ** 5 for s in second_sequence)
 
-    type here
+    sum_product = 0
+    first_sequence.sort(reverse=True)  # O(n log n) - sort both sequences in descending order
+    second_sequence.sort(reverse=True)  # O(n log n)
+    for i in range(0,len(first_sequence)):  # O(n)
+        sum_product += first_sequence[i] * second_sequence[i]  # - calculate the dot product
+    return sum_product
 
 
 if __name__ == '__main__':
@@ -32,3 +37,7 @@ if __name__ == '__main__':
     clicks = list(map(int, input().split()))
     assert len(prices) == len(clicks) == n
     print(max_dot_product(prices, clicks))
+
+# print(max_dot_product([2, 1], [5, 10]))
+# print(max_dot_product([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]))
+# print(max_dot_product([17, 12, 20], [19, 2, 3]))
