6fda09f94dfefd8654476dc78ac9d1e6c7d3634f
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-05-07 10:51:35 -0600

    Added code to sort key for edge case (1000)

--- Greedy Algorithms/Maximum Salary/maximum_salary.py
+++ Greedy Algorithms/Maximum Salary/maximum_salary.py
@@ -15,7 +15,7 @@ def largest_number_naive(numbers):
 
 
 def sort_key(n):  # sort order for largest number problem
-    # assert 0 <= n < 999
+    # assert 0 <= n < 10**3
     n = str(n)  #convert number to string
     if len(n) == 1:  # if a single digit number n
         return int(n + n + n)  # convert to three digit nnn
@@ -23,20 +23,24 @@ def sort_key(n):  # sort order for largest number problem
         return int(n + n[0])  # convert to three digit nmn
     elif len(n) == 3:  # if a three digit number nmo
         return int(n)  # return nmo
+    elif len(n) == 4:  # if a four digit number 1000
+        return int(0)  # return 0
 
 
-def largest_number(numbers):  # parametr is list of integers 1<= n <= 999
+
+def largest_number(numbers):  # parameter is list of integers 1<= n <= 999
     numbers.sort(key=sort_key, reverse=True)  # sort descending by sort function
     largest = list(map(str, numbers))  # convert integer list to list of strings
     largest = "".join(largest)  # concatenate
     return int(largest)  # return as an integer
 
+#
+# if __name__ == '__main__':
+#     n = int(input())
+#     input_numbers = input().split()
+#     assert len(input_numbers) == n
+#     print(largest_number(input_numbers))
 
-if __name__ == '__main__':
-    n = int(input())
-    input_numbers = input().split()
-    assert len(input_numbers) == n
-    print(largest_number(input_numbers))
 
-
-
+# print(largest_number_naive([1, 11, 13, 111, 113, 131, 132, 133, 1000]))
+# print(largest_number([1, 11, 13, 111, 113, 131, 132, 133, 1000]))
