a3f5be5ad95cc008ce4274fafa3213ab39785b60
middleagedspread <103695172+middleagedspread@users.noreply.github.com>
2022-06-10 21:55:44 -0600

    Replace dynamic algorithm for three-way equal sum partition of set of integers

--- Dynamic Programming/Partitioning Souvenirs/partition_souvenirs.py
+++ Dynamic Programming/Partitioning Souvenirs/partition_souvenirs.py
@@ -13,63 +13,53 @@ def partition3_naive(values):
 
     return 0
 
-# Function for solving the 3–partition problem.
-# It returns true if there exist three subsets with the given sum
-def subsetSum(values, index_of_next_integer, subset_sum_a, subset_sum_b, subset_sum_c, lookup):
 
-    # return true if the subset is found
-    if subset_sum_a == 0 and subset_sum_b == 0 and subset_sum_c == 0:
-        return True
-
-    # base case: no items left
-    if index_of_next_integer < 0:
-        return False
-
-    # construct a unique key from dynamic elements of the input
-    key = (subset_sum_a, subset_sum_b, subset_sum_c, index_of_next_integer)
-
-    # if the sub-problem is seen for the first time, solve it and
-    # store its result in a dictionary
-    if key not in lookup:
-
-        # Case 1. The current item becomes part of the first subset
-        A = False
-        if subset_sum_a - values[index_of_next_integer] >= 0:  # if this integer is less than the remaining subset_sum
-            # recursively call the algorithm with the integer index decremented and the subset_sum reduced
-            A = subsetSum(values, index_of_next_integer - 1, subset_sum_a - values[index_of_next_integer], subset_sum_b, subset_sum_c, lookup)
-
-        # Case 2. The current item becomes part of the second subset
-        B = False
-        if not A and (subset_sum_b - values[index_of_next_integer] >= 0):  # only called if A is false and this integer < remaining subset_sum
-            B = subsetSum(values, index_of_next_integer - 1, subset_sum_a, subset_sum_b - values[index_of_next_integer], subset_sum_c, lookup)
-
-        # Case 3. The current item becomes part of the third subset
-        C = False
-        if (not A and not B) and (subset_sum_c - values[index_of_next_integer] >= 0):
-            C = subsetSum(values, index_of_next_integer - 1, subset_sum_a, subset_sum_b, subset_sum_c - values[index_of_next_integer], lookup)
-
-        # return true if we get a solution
-        lookup[key] = A or B or C
-
-    # return the sub-problem solution from the dictionary
-    return lookup[key]
-
-def partition3(values):
+def partition3(values, k=3):
+    values = list(values)
     assert 1 <= len(values) <= 20
     assert all(1 <= v <= 30 for v in values)
 
-    if len(values) < 3:
+    total_sum = sum(values)
+    if total_sum % k != 0:  # if the sum is not divisible by k return False
+        return False * 1
+    target = total_sum // k
+    used = [False]*len(values)  # initialise list to flag whether integer has been used
+
+    # sort the array in descending order
+    # if the first value is greater than target we cannot partition
+    # the array into equal sum subsets == target
+    values.sort(reverse=True)
+    if values[0] > target:
+        return False * 1
+
+    # to reduce the number of recursive calls we will memoise
+    # used
+    dp = {}
+
+    def backtrack(index, remaining_partitions, subset_sum):
+        # since sub-problem depends on used indices of array
+        # if same sub-problem occurs again just return dp value
+        # print("tuple(used):",tuple(used))
+        # for i in dp:
+        #     print("dp:",i)
+        if tuple(used) in dp:
+            return dp[tuple(used)]
+        if remaining_partitions == 0:
+            return True
+        if subset_sum == 0:
+            partition = backtrack(0, remaining_partitions-1, target)
+            dp[(tuple(used), subset_sum)] = partition
+            return partition
+        for j in range(index, len(values)):
+            if not used[j] and subset_sum-values[j] >= 0:
+                used[j] = True
+                if backtrack(j+1, remaining_partitions, subset_sum-values[j]):
+                    return True
+                used[j] = False
+        dp[tuple(used)] = False
         return False
 
-    # create a dictionary to store solutions to a sub-problem
-    lookup = {}
-
-    # get the sum of all elements in the set
-    total = sum(values)
-
-    # return true if the sum is divisible by 3 and the set of `values`
-    # can be divided into three subsets with an equal sum
-    return 1 * ((total % 3) == 0 and subsetSum(values, len(values) - 1, total//3, total//3, total//3, lookup))
+    return 1 * backtrack(0, k, target)
 
 
 if __name__ == '__main__':
@@ -77,7 +67,7 @@ if __name__ == '__main__':
     assert input_n == len(input_values)
     print(partition3(input_values))
 
-# values = (3, 4, 1, 6, 2, 5)
+# nums = (3, 4, 1, 6, 2, 5)
 #
-# print(partition3_naive(values))
-# print(partition3(values))
+# print(partition3_naive(nums))
+# print(partition3(nums))
